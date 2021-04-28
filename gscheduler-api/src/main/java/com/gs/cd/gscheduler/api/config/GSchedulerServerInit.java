0f779d0959fcb7d8b02b71d4efccce3dce8f5d31
yijialuo <yijialuo@gridsum.com>
2021-04-28 18:02:10 +0800

    获取有效租户逻辑修改

--- gscheduler-api/src/main/java/com/gs/cd/gscheduler/api/config/GSchedulerServerInit.java
+++ gscheduler-api/src/main/java/com/gs/cd/gscheduler/api/config/GSchedulerServerInit.java
@@ -31,43 +31,47 @@ public class GSchedulerServerInit implements ApplicationRunner {
     @Autowired
     AuthClient authClient;
 
-    public void TriggerInit() {
+    public void triggerInit() throws Exception {
         log.info("*******\tinit GSchedulerTrigger corn task\t*******");
-        ApiResult apiResult = authClient.listAllValidTenantCode();
-        if (!apiResult.isSuccess()) {
-            log.error("调用 authClient.listAllValidTenantCode 错误：" + apiResult);
-            System.exit(-1);
-        }
-        // TODO: 2021/4/28 记录一个可能得问题  如果说 sys_tenant_own_store 中的租户不在同一个pg上 可能存在查询失败的问题
-        HashMap<String, String> errorMsg = new HashMap<>();
-        JSONUtil.parseArray(apiResult.getData()).forEach(tenantCode -> {
-            List<GschedulerTrigger> gschedulerTriggers = new ArrayList<>();
-            try {
-                gschedulerTriggers = gschedulerTriggerService.listByTenantCode(tenantCode.toString());
-            } catch (Exception e) {
-                errorMsg.put(tenantCode.toString(), e.getMessage());
+        try {
+            ApiResult apiResult = authClient.listAllValidTenantCode();
+            if (!apiResult.isSuccess()) {
+                log.error("调用 authClient.listAllValidTenantCode 错误：" + apiResult);
+                System.exit(-1);
             }
-            gschedulerTriggers.forEach(t -> {
+            // TODO: 2021/4/28 记录一个可能得问题  如果说 sys_tenant_own_store 中的租户不在同一个pg上 可能存在查询失败的问题
+            HashMap<String, String> errorMsg = new HashMap<>();
+            JSONUtil.parseArray(apiResult.getData()).forEach(tenantCode -> {
+                List<GschedulerTrigger> gschedulerTriggers = new ArrayList<>();
                 try {
-                    quartzExecutors.addJob(tenantCode.toString(), t);
+                    gschedulerTriggers = gschedulerTriggerService.listByTenantCode(tenantCode.toString());
                 } catch (Exception e) {
-                    errorMsg.put(String.format("tenantCode=%s data=> %s", tenantCode.toString(), t.toString()), e.getMessage());
+                    errorMsg.put(tenantCode.toString(), e.getMessage());
                 }
+                gschedulerTriggers.forEach(t -> {
+                    try {
+                        quartzExecutors.addJob(tenantCode.toString(), t);
+                    } catch (Exception e) {
+                        errorMsg.put(String.format("tenantCode=%s data=> %s", tenantCode.toString(), t.toString()), e.getMessage());
+                    }
+                });
             });
-        });
-        if (!errorMsg.isEmpty()) {
-            log.error("!!!!!!!!!!!!!!!初始化触发器错误：!!!!!!!!!!!!!!");
-            errorMsg.forEach((k, v) -> {
-                log.error("错误数据:{}，错误详情:{}", k, v);
-            });
+            if (!errorMsg.isEmpty()) {
+                log.error("!!!!!!!!!!!!!!!初始化触发器错误：!!!!!!!!!!!!!!");
+                errorMsg.forEach((k, v) -> {
+                    log.error("错误数据:{}，错误详情:{}", k, v);
+                });
+                System.exit(-1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
             System.exit(-1);
         }
     }
 
     @Override
     public void run(ApplicationArguments arg0) throws Exception {
-        TriggerInit();
-
+        triggerInit();
     }
 
 }
