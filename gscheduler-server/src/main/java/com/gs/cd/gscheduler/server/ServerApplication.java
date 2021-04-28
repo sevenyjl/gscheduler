e08fcde5107c52405ffca74118a00169e34c1b75
yijialuo <yijialuo@gridsum.com>
2021-04-28 15:39:23 +0800

    调整pom依赖

---
+++ gscheduler-server/src/main/java/com/gs/cd/gscheduler/server/ServerApplication.java
@@ -0,0 +1,14 @@
+package com.gs.cd.gscheduler.server;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author seven
+ * @Date 2021/4/28 15:34
+ * @Description
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class ServerApplication {
+
+}
