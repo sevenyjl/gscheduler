6d544c314071ffcc973c095751919262614a0dba
yijialuo <yijialuo@gridsum.com>
2021-05-17 13:52:06 +0800

    no message

--- gscheduler-server/部署/gscheduler-server.sql
+++ gscheduler-server/部署/gscheduler-server.sql
@@ -1,5 +1,8 @@
 -- ----------------------------
--- Table structure for gscheduler_project_purview
+-- 2021年5月17日 13:50:09
+-- kipf v2.7.2
+-- gscheduler v1.1.0
+-- Developer 易佳骆
 -- ----------------------------
 DROP TABLE IF EXISTS "developer_gs"."gscheduler_project_purview";
 CREATE TABLE "developer_gs"."gscheduler_project_purview"
@@ -10,9 +13,8 @@ CREATE TABLE "developer_gs"."gscheduler_project_purview"
     "role_id"       varchar(255) COLLATE "pg_catalog"."default"
 )
 ;
-
--- ----------------------------
--- Primary Key structure for table gscheduler_project_purview
--- ----------------------------
 ALTER TABLE "developer_gs"."gscheduler_project_purview"
     ADD CONSTRAINT "gscheduler_project_purview_pkey" PRIMARY KEY ("id");
+-- ----------------------------
+-- 【END】gscheduler
+-- ----------------------------
